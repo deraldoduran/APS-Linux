2f5500144e17cd6a27845d3b637e823e53e9f680
deraldoduran <deraldoduran@gmail.com>
2020-12-04 18:17:22 -0300

    agora com o git resetado

--- RevendaComArray.java
+++ RevendaComArray.java
@@ -16,26 +16,6 @@ import java.util.ArrayList;
  */
 public class RevendaComArray {
     private ArrayList<Produto> produto;
-
-<<<<<<< HEAD
-    
-=======
-    public ArrayList<Produto> getProduto() {
-        return produto;
-    }
-
-    public void setProduto(ArrayList<Produto> produto) {
-        this.produto = produto;
-    }
-
-    public int getIndice() {
-        return indice;
-    }
-
-    public void setIndice(int indice) {
-        this.indice = indice;
-    }
->>>>>>> 0ac34fd1abef656daea228b58aa9993ad2d1be3a
     private int indice;
 
     /*private int codigoProduto;
@@ -49,7 +29,6 @@ public class RevendaComArray {
     */
 
     public RevendaComArray(int indice ) {
-<<<<<<< HEAD
         
         this.produto = new ArrayList (indice);
         for(int i = 0;i<indice; i++){
@@ -80,32 +59,6 @@ public class RevendaComArray {
     
     
 //}
-=======
-        this.produto = new ArrayList (indice);
-
-    }
-
-    public void inserirProduto(int indice, Produto codigoProduto, Produto descricaoProduto, Produto valorCompra, Produto custoArmazenagemouBeneficiamento, Produto margemLucro, Produto  qtdEstoque){
-        produto.set(indice, new Produto(codigoProduto, descricaoProduto, valorCompra, custoArmazenagemouBeneficiamento, margemLucro, qtdEstoque));
-    }
-    
-    public void comprar(int code, int qtdcomprada){
-        int cont = 0;
-        for(int i = 0; i<this.indice; i++){
-        if(this.produto.get(i).getCodigoProduto().equals(code)){
-            this.produto.get(i).compra(qtdcomprada);
-        }if (this.produto.get(i).getCodigoProduto() != code){
-            cont++;
-        }
-            }
-        if(cont == this.indice){
-            System.out.println("Esse produto nao existe!");
-        }
-    }
-    
-    
-}
->>>>>>> 0ac34fd1abef656daea228b58aa9993ad2d1be3a
     public void inserirProduto(int indice, Produto codigoProduto, Produto descricaoProduto, Produto valorCompra, Produto custoArmazenagemouBeneficiamento, Produto margemLucro, Produto  qtdEstoque){
         
         this.produto.set(indice, codigoProduto);
@@ -127,6 +80,6 @@ public class RevendaComArray {
         }
         if(cont == this.indice){
             System.out.println("Produto Inexistente, tente outro código!");
-        }
     }
+}
 }
